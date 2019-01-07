a26e497dcc25cf2f60ed421f740106b196cd74f3
Nikita Braslavskiy <nikita.braslavskiy@gmail.com>
2019-01-07 23:39:58 +0200

    nm: fixed section indexing

--- ft_nm/src/sections_index.c
+++ ft_nm/src/sections_index.c
@@ -4,7 +4,35 @@
 #include "libft.h"
 #include "nm.h"
 
-static void read_segment(struct load_command *lc) {
+static void read_segment32(struct load_command *lc) {
+
+    struct segment_command *segment;
+    struct section         *section;
+    index_t                *index;
+    uint32_t               i;
+
+    segment = (struct segment_command *)lc;
+    section = (struct section *)((void*)segment + sizeof(*segment));
+    index = get_sect_index();
+
+    i = 0;
+    while (i < segment->nsects) {
+        if (!ft_strcmp(section->segname, SEG_TEXT) && \
+            !ft_strcmp(section->sectname, SECT_TEXT))
+            index->text = index->nsects + 1;
+        else if (!ft_strcmp(section->segname, SEG_DATA) && \
+                 !ft_strcmp(section->sectname, SECT_DATA))
+            index->data = index->nsects + 1;
+        else if (!ft_strcmp(section->segname, SEG_DATA) && \
+                 !ft_strcmp(section->sectname, SECT_BSS))
+            index->bss = index->nsects + 1;
+        index->nsects++;
+        section++;
+        i++;
+    }
+}
+
+static void read_segment64(struct load_command *lc) {
 
     struct segment_command_64 *segment;
     struct section_64         *section;
@@ -42,8 +70,10 @@ void index_sections(uint32_t ncmds, void *lc_start) {
 
     i = 0;
     while (i < ncmds) {
-        if (lc->cmd == LC_SEGMENT_64)
-            read_segment(lc);
+        if (lc->cmd == LC_SEGMENT)
+            read_segment32(lc);
+        else if (lc->cmd == LC_SEGMENT_64)
+            read_segment64(lc);
         lc = (void *)lc + lc->cmdsize;
         i++;
     }
