5d236984044fb9fb4553d23833a021963fdb33a2
Nikita Braslavskiy <nikita.braslavskiy@gmail.com>
2019-01-23 10:35:32 +0200

    otool: removed unused stuff

--- ft_otool/include/ft_otool.h
+++ ft_otool/include/ft_otool.h
@@ -23,23 +23,6 @@ typedef struct  otool_obj {
     int         fd;
 }               otool_obj_t;
 
-typedef struct  symtab {
-    uint8_t     *n_name;
-    uint8_t     n_type;
-    uint8_t     n_sect;
-    uint16_t    n_desc;
-    uint64_t    n_value;
-}               symtab_t;
-
-typedef struct  section_index {
-    uint8_t     nsects;
-    uint8_t     text;
-    uint8_t     data;
-    uint8_t     bss;
-}               index_t;
-
-index_t *get_sect_index(void);
-
 int     otool_read_file(const char *path, void *ptr, size_t size);
 
 int     handle_macho32(const char *name, void *ptr);
@@ -50,11 +33,6 @@ int     handle_ar(const char *name, void *ptr, size_t size);
 void    get_text_sect32(void *ptr);
 void    get_text_sect64(void *ptr);
 int     read_fat(const char *name, void *ptr);
-
-void    index_sections(uint32_t ncmds, void *lc_start);
-index_t *get_sect_index(void);
-
-void    sort_symtab(struct symtab *arr, size_t n);
 
 void    print_filename(const char *name, const char *sub, uint32_t arch);
 
